6c2dce04a564948f34cc328d97a65a8546f791cd
Zhang Zhihui <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-14 10:55:13 -0400

    Update files

--- update_readme.py
+++ update_readme.py
@@ -15,10 +15,11 @@ modules.sort()
 w = open('README.md', 'w')
 print('# {}'.format(repo_name), file=w)
 print('\n', file=w)
-fmt_str = '{:30s}{}'
+fmt_str = '<pre>{:30s}{}</pre>'
 print(fmt_str.format('Script Name', 'Summary' ), file=w)
 print('-' * 100, file=w)
 for m in modules:
     m_imp = importlib.import_module(m)
     print(fmt_str.format(m + '.py', m_imp.__doc__), file=w)
 print('-' * 100, file=w)
+w.close()
