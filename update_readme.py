6ea48d3e06a5c41fb188c17f223617def8f7f8f1
Zhang Zhihui <30534140+ZhangZhihuiAAA@users.noreply.github.com>
2018-06-14 10:15:40 -0400

    Add new files

---
+++ update_readme.py
@@ -0,0 +1,24 @@
+"""Update README.md.
+
+"""
+
+import os
+import sys
+import importlib
+
+repo_name = os.path.basename(os.getcwd())
+
+files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.py')]
+modules = [f.split('.')[0] for f in files if f != 'update_readme.py']
+modules.sort()
+
+w = open('README.md', 'w')
+print('# {}'.format(repo_name), file=w)
+print('\n', file=w)
+fmt_str = '{:30s}{}'
+print(fmt_str.format('Script Name', 'Summary' ), file=w)
+print('-' * 100, file=w)
+for m in modules:
+    m_imp = importlib.import_module(m)
+    print(fmt_str.format(m + '.py', m_imp.__doc__), file=w)
+print('-' * 100, file=w)
